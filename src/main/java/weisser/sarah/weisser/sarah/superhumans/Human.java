f3abf3c271be4b85269a68d0e9fa6dee2c9bac07
Sarah Weisser <sarahweisser@zipcoder23.local>
2017-05-01 16:51:58 -0400

    added human class and printfields method

---
+++ src/main/java/weisser/sarah/weisser/sarah/superhumans/Human.java
@@ -0,0 +1,65 @@
+package weisser.sarah.weisser.sarah.superhumans;
+
+/**
+ * Created by sarahweisser on 5/1/17.
+ */
+public class Human {
+
+    private String name;
+    private int age;
+    private String gender;
+    private String occupation;
+    private String address;
+
+    public Human(String name, int age, String gender, String occupation, String address) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAddress() {
+        return address;
+    }
+
+    public void printFields() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Occupation: " + occupation);
+        System.out.println("Address: " + address);
+    }
+
+}
---
+++ src/main/java/weisser/sarah/weisser/sarah/superhumans/Main.java
@@ -0,0 +1,13 @@
+package weisser.sarah.weisser.sarah.superhumans;
+
+/**
+ * Created by sarahweisser on 5/1/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Human joe = new Human("Joe", 33, "M", "Software Developer", "Arden");
+        joe.printFields();
+
+    }
+}
